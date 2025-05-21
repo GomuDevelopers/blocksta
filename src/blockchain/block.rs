95ef9c908226f1c1289ab4c0c0542190e38d30f6
KATIKA <mouliofitzgerard@gmail.com>
2025-05-21 10:02:25 +0100

    fix: compiler time bug on block.rs and pow

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -1,7 +1,6 @@
 extern crate bincode;
 use super::proofofwork::ProofOfWork;
 use super::transaction::Transaction;
-use bincode::{deserialize, serialize};
 use serde::{Deserialize, Serialize};
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::{u8, usize};
@@ -11,34 +10,74 @@ pub struct Block {
     timestamp: u64,
     pre_block_hash: String,
     hash: String,
-    transaction: Vec<Transaction>,
+    transactions: Vec<Transaction>,
     nonce: i64,
     height: usize,
 }
 
 impl Block {
-    pub fn new_block(pre_block_hash: String, transactions: &[Transaction], height: usize) -> Block {
-        let mut block = Block {
+    pub fn new_block(
+        pre_block_hash: String,
+        trasactions_input: &[Transaction],
+        height: usize,
+    ) -> Block {
+        let block = Block {
             timestamp: SystemTime::now()
                 .duration_since(UNIX_EPOCH)
                 .unwrap()
                 .as_secs(),
             pre_block_hash,
             hash: String::new(),
-            transaction: transactions.to_vec(),
+            transactions: transactions_input.to_vec(),
             nonce: 0,
             height,
         };
-        let pow = ProofOfWork::new_proof_of_work(&block);
+
+        let pow = ProofOfWork::new_proof_of_work(block);
         let (nonce, hash) = pow.run();
         block.nonce = nonce;
         block.hash = hash;
         return block;
     }
     pub fn deserialization(bytes: &[u8]) -> Block {
-        deserialize(bytes).unwrap()
+        bincode::deserialize(bytes).unwrap()
     }
     pub fn serialization(&self) -> Vec<u8> {
-        serialize(self).unwrap().to_vec()
+        bincode::serialize(self).unwrap().to_vec()
+    }
+
+    pub fn generate_genesis_block(transaction: &Transaction) -> Block {
+        let transactions = vec![transaction.clone()];
+        Block::new_block(String::from("None"), &transactions, 0)
+    }
+    pub fn hash_transaction(&self) -> Vec<u8> {
+        let mut txhashs = vec![];
+        for transaction in self.transactions {
+            txhashs.extend(transaction.get_id());
+        }
+    }
+
+    pub fn get_transactions(&self) -> &[Transaction] {
+        self.transactions.as_slice()
+    }
+
+    pub fn get_pre_block_hash(&self) -> &str {
+        self.pre_block_hash.as_ref()
+    }
+
+    pub fn get_hash(&self) -> &str {
+        self.hash.as_ref()
+    }
+
+    pub fn get_hash_bytes(&self) -> Vec<u8> {
+        self.hash.as_bytes().to_vec()
+    }
+
+    pub fn get_timestamp(&self) -> u64 {
+        self.timestamp
+    }
+
+    pub fn get_height(&self) -> usize {
+        self.height
     }
 }
--- src/blockchain/proofofwork.rs
+++ src/blockchain/proofofwork.rs
@@ -1,41 +1,66 @@
 use std::usize;
 
 use super::block::Block;
+use data_encoding::HEXLOWER;
 use num_bigint::{BigInt, Sign};
-#[derive(Debug)]
+use ring::digest;
+use std::ops::ShlAssign;
+
+#[derive(Debug, Clone)]
 pub struct ProofOfWork {
     block: Block,
-    target: &BigInt,
+    target: BigInt,
     //the target is required for mining, where the target is a mathematical result of a formula converted into
     //a hexadecimal number that dictates the mining difficulty/
 }
 
-impl ProofOfWork {
-    pub fn prepare_digest(nonce: usize) -> Vec<u32> {}
+const TARGET_BITS: i32 = 8;
+const MAX_NONCE: i64 = i64::MAX;
 
-    pub fn sha256_digest() {}
+impl ProofOfWork {
+    pub fn new_proof_of_work(block: Block) -> ProofOfWork {
+        let mut target = BigInt::from(1);
+
+        target.shl_assign(256 - &TARGET_BITS);
+        return ProofOfWork {
+            block: block,
+            target: target,
+        };
+    }
+
+    pub fn prepare_data(&self, nonce: i64) -> Vec<u8> {
+        let pre_block_hash = self.block.get_pre_block_hash();
+        let transaaction_hash = self.block.hash_transaction();
+        let timestamp = self.block.get_timestamp();
+        let mut data_bytes = vec![];
+        data_bytes.extend(pre_block_hash.as_bytes());
+        data_bytes.extend(transaaction_hash);
+        data_bytes.extend(timestamp.to_be_bytes());
+        data_bytes.extend(TARGET_BITS.to_be_bytes());
+        data_bytes.extend(nonce.to_be_bytes());
+        data_bytes
+    }
 
     pub fn run(&self) -> (i64, String) {
-        const MAX_NONCE: u32 = 100;
         let mut nonce = 0;
         let mut hash = Vec::new();
         println!("Mining the block");
         while nonce < MAX_NONCE {
-            let data = self.prepare_digest(nonce);
-            hash = crate::sha256_digest(data.as_slice());
-            let hash_int = BigInt::from_bytes_be(Sign::Plus, bytes);
+            let data = self.prepare_data(nonce);
+            hash = digest::digest(&digest::SHA256, data.as_slice())
+                .as_ref()
+                .to_vec();
 
-            if hash_int.lt(self.target) {
+            let hash_int = BigInt::from_bytes_be(Sign::Plus, hash.as_slice());
+
+            if hash_int.lt(&self.target) {
                 println!("{}", HEXLOWER.encode(hash.as_slice()));
                 break;
             } else {
                 nonce += 1;
-                self.prepare_digest()
             }
         }
         println!(" Block running ");
         return (nonce, HEXLOWER.encode(hash.as_slice()));
     }
-
-    pub fn new_proof_of_work(block: &Block) -> ProofOfWork {}
 }
